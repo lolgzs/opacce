e2e6524efcbaa54e80b131f89cc61c2433e0ae1e
pbarroca <pbarroca@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-04-19 16:07:22 +0000

    OAI: Début controller oai
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@68 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

---
+++ application/modules/opac/controllers/OaiController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright (c) 2012, Agence Française Informatique (AFI). All rights reserved.
+ *
+ * AFI-OPAC 2.0 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation.
+ *
+ * There are special exceptions to the terms and conditions of the AGPL as it
+ * is applied to this software (see README file).
+ *
+ * AFI-OPAC 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with AFI-OPAC 2.0; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA 
+ */
+class OaiController extends Zend_Controller_Action {
+	public function requestAction() {
+		$this->getResponse()->setHeader('Content-Type', 'text/xml;charset=utf-8');
+		$this->getHelper('ViewRenderer')->setNoRender();
+
+		$baseUrl = $this->_request->getScheme() . ':' . $_SERVER['SERVER_NAME'] 
+			. BASE_URL . '/opac/oai/request';
+		$response = new Class_WebService_OAI_Response_Identify($baseUrl);
+		$this->_response->setBody($response->xml());
+	}
+}
+
+?>
