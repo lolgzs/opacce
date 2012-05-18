407d065c31bbc22550f0267e8c84ce539988091f
llaffont <llaffont@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-05-18 14:23:24 +0000

    Regroupement des entrées de menu relatives à la bibliothèque numérique
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@225 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

--- library/ZendAfi/View/Helper/Admin/MenuGaucheAdmin.php
+++ library/ZendAfi/View/Helper/Admin/MenuGaucheAdmin.php
@@ -44,19 +44,25 @@ class ZendAfi_View_Helper_Admin_MenuGaucheAdmin extends ZendAfi_View_Helper_Base
 		$menu_modules .= $this->addMenu("catalogues.png",	$this->translate()->_("Catalogues dynamiques"),		"/admin/catalogue",					 $acl_admins);
 		$menu_modules .= $this->addMenu("rss.gif",				$this->translate()->_("Fils RSS"),								"/admin/rss",								 $acl_admins);
 		$menu_modules .= $this->addMenu("web.gif",				$this->translate()->_("Sitothèque"),							"/admin/sito",							 $acl_admins);
-		if (Class_AdminVar::isBibNumEnabled()) {
-			$menu_modules .= $this->addMenu("images.png",		$this->translate()->_("Bibliothèque numérique"),  "/admin/album",							 $acl_admins);
-		}
 		$menu_modules .= $this->addMenu("moderation.gif",	$this->translate()->_("Modération"),							"/admin/modo/",							 $acl_all,								 $nb_moderations);
     $menu_modules .= $this->addMenu("vcard.png",			$this->translate()->_("Demandes d'inscription"),	"/admin/modo/membreview",		 $acl_all,								 $demandes_inscription);
 		$menu_modules .= $this->addMenu("mail.png",				$this->translate()->_("Lettres d'information"),		"/admin/newsletter",				 $acl_admins);
-
 		if (Class_AdminVar::isFormationEnabled()) {
 			$menu_modules .= $this->addMenu("formation.png", $this->translate()->_("Formations"),							"/admin/formation",					 $acl_admins);
 			$menu_modules .= $this->addMenu("compass.png",   $this->translate()->_("Lieux"),				  		  	"/admin/lieu",	 		  		   $acl_admins);
 		}
-
 		$menu_modules .= $this->closeBoite();
+
+
+		$menu_bibnum = '';
+		if (Class_AdminVar::isBibNumEnabled()) {
+			$menu_bibnum .= $this->openBoite($this->translate()->_("Bibliothèque numérique"));
+			$menu_bibnum .= $this->addMenu("images.png",		$this->translate()->_("Collections"),           "/admin/album",							   $acl_admins);
+			$menu_bibnum .= $this->addMenu("epub.png",		  $this->translate()->_("Ressources OPDS"),				"/admin/opds",                 $acl_admins);
+			$menu_bibnum .= $this->addMenu("website.gif",	$this->translate()->_("Ressources OAI"),				  "/admin/systeme/ressourcesoai",$acl_admins);
+			$menu_bibnum .= $this->closeBoite();
+		}
+
 
 		$menu_page  = $this->openBoite($this->translate()->_("Mise en page"));
 		$menu_page .= $this->addMenu("ecran.png",			$this->translate()->_("Profils"),									"/admin/profil",				 $acl_admins);
@@ -64,7 +70,6 @@ class ZendAfi_View_Helper_Admin_MenuGaucheAdmin extends ZendAfi_View_Helper_Base
 
 		if (Class_AdminVar::isTranslationEnabled())
 			$menu_page .= $this->addMenu("catalogues.png",		$this->translate()->_("Traductions"),	"/admin/i18n",	 $acl_admins);
-
 
 		$menu_page .= $this->closeBoite();
 
@@ -96,14 +101,12 @@ class ZendAfi_View_Helper_Admin_MenuGaucheAdmin extends ZendAfi_View_Helper_Base
     $menu_systeme .= $this->addMenu("systeme.png",		$this->translate()->_("Variables"),								"/admin/index/adminvar",			 $acl_super_admin);
 		$menu_systeme .= $this->addMenu("tester.gif",			$this->translate()->_("Test des web-services"),		"/admin/systeme/webservices",	 $acl_super_admin);
 		$menu_systeme .= $this->addMenu("images.png",			$this->translate()->_("Cache des images"),				"/admin/systeme/cacheimages",	 $acl_super_admin);
-		$menu_systeme .= $this->addMenu("website.gif",		$this->translate()->_("Ressources OAI"),					"/admin/systeme/ressourcesoai",$acl_admins_portail);
-		$menu_systeme .= $this->addMenu("epub.png",		$this->translate()->_("Ressources OPDS"),					"/admin/opds",$acl_admins_portail);
 		$menu_systeme .= $this->addMenu("chat.gif.png",		$this->translate()->_("Import avis opac2"),					"/admin/systeme/importavisopac2",$acl_super_admin);
 		$menu_systeme .= $this->closeBoite();
 
 		// Activation des menus en fonction du rôle
-		$html_menu=$menu_modules;
-		if (in_array($this->user->ROLE, $acl_admins)) $html_menu .= $menu_page;
+		$html_menu = $menu_modules;
+		if (in_array($this->user->ROLE, $acl_admins)) $html_menu .= $menu_bibnum.$menu_page;
 		$html_menu .= $menu_stat;
 		if (in_array($this->user->ROLE, $acl_admins)) $html_menu .= $menu_portail;
 		if (in_array($this->user->ROLE, $acl_admins_portail)) $html_menu .= $menu_systeme;
