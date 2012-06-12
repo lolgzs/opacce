0f2009947ff3d5f6fee64b25914816f70e769cc7
llaffont <llaffont@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-06-12 15:24:13 +0000

    Ajout helper pour afficher des liens sur le profil pour les réseaux sociaux
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@411 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

---
+++ library/ZendAfi/View/Helper/SocialShare.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) 2012, Agence Française Informatique (AFI). All rights reserved.
+ *
+ * AFI-OPAC 2.0 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation.
+ *
+ * There are special exceptions to the terms and conditions of the AGPL as it
+ * is applied to this software (see README file).
+ *
+ * AFI-OPAC 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with AFI-OPAC 2.0; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA 
+ */
+
+class ZendAfi_View_Helper_SocialShare extends Zend_View_Helper_HtmlElement {
+	public function socialShare($profil, $networks) {
+		$this->addHeadScript();
+
+		$html = '';
+		foreach ($networks as $network) 
+			$html .= $this->htmlForNetwork($profil, $network);
+
+		return '<div class="share">'.$html.'</div>';
+	}
+
+
+	public function htmlForNetwork($profil, $network) {
+		return  sprintf('<img class="%s" src="%s"  onclick="socialShare(\'%s\')" alt="%s"/>',
+										$network,
+										URL_IMG.'/site/'.$network.'.png',										
+										$network,
+										'partager sur '.$network);
+	}
+
+
+	public function addHeadScript() {
+		Class_ScriptLoader::getInstance()
+			->addInlineScript(sprintf("function socialShare(network) {"
+																."jQuery.getScript('%s/index/share/on/' + network + '/titre/' + $(\"title\").text());"
+																."}",
+																BASE_URL));
+	}
+}
+
+?>
