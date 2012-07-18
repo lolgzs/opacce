776ee73777583ac285779ed71e5204e0a9a7d584
pbarroca <pbarroca@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-07-18 13:49:19 +0000

    MAJ version 5.21.3 Hotfix syntaxe fulltext des catalogues dynamiques
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@593 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

--- library/startup.php
+++ library/startup.php
@@ -51,7 +51,7 @@ function defineConstant($name, $value) {
 
 
 function setupConstants($cfg) {
-	defineConstant('VERSION_PERGAME','V-05.21.2 [r'.array_at(1, explode(' ', '$Revision$ ')).']');
+	defineConstant('VERSION_PERGAME','V-05.21.3 [r'.array_at(1, explode(' ', '$Revision$ ')).']');
 	defineConstant('ROOT_PATH',  './');
 
 	defineConstant('MODULEDIRECTORY','./application/modules');
