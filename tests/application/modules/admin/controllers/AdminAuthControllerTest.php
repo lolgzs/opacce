9faf47a2d3f03409865fb5849456ea495de91cb6
llaffont <llaffont@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-08-22 13:15:14 +0000

    Test authentification admin
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@747 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

---
+++ tests/application/modules/admin/controllers/AdminAuthControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright (c) 2012, Agence Française Informatique (AFI). All rights reserved.
+ *
+ * AFI-OPAC 2.0 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation.
+ *
+ * There are special exceptions to the terms and conditions of the AGPL as it
+ * is applied to this software (see README file).
+ *
+ * AFI-OPAC 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with AFI-OPAC 2.0; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA 
+ */
+require_once 'AdminAbstractControllerTestCase.php';
+
+class AdminAuthControllerTest extends Admin_AbstractControllerTestCase {
+	protected $_auth;
+	protected $_auth_db_adapter;
+
+	public function setUp() {
+		parent::setUp();
+		$this->_auth_db_adapter = Storm_Test_ObjectWrapper::mock();
+		$this->_auth = Storm_Test_ObjectWrapper::mock()
+			->whenCalled('authenticateLoginPassword')
+			->answers(false)
+			->whenCalled('hasIdentity')
+			->answers(false)
+			->whenCalled('getIdentity')
+			->answers(null)
+			->whenCalled('newAuthDb')
+			->answers($this->_auth_db_adapter);
+		
+		ZendAfi_Auth::setInstance($this->_auth);
+	}
+
+
+	/** @test */
+	public function withAuthenticationSuccessfullShouldRedirectToAdmin() {
+		$this->_auth
+			->whenCalled('authenticateLoginPassword')
+			->with('foo', 'bar', [$this->_auth_db_adapter])
+			->answers(true);
+
+		$this->postDispatch('/admin/auth/login',
+												['username' => 'foo', 'password' => 'bar']);
+
+		$this->assertRedirectTo('/admin/');
+	}
+
+
+	/** @test */
+	public function withAuthenticationFailureShouldNotRedirectToAdmin() {
+		$this->postDispatch('/admin/auth/login',
+												['username' => 'foo', 'password' => 'bar']);
+
+		$this->assertNotRedirect('/admin/');
+	}
+
+	/** @test */
+	public function withNoUsernameShouldDisplayMessageEntrezVotreNom() {
+		$this->postDispatch('/admin/auth/login', []);
+		$this->assertXPathContentContains('//span', 'Entrez votre nom d\'utilisateur');
+	}
+
+
+	public function tearDown() {
+		ZendAfi_Auth::setInstance(null);
+		parent::tearDown();
+	}
+}
+
+?>
