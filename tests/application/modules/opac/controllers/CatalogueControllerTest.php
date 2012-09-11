58f39e590eefcab5c14ec62e6c01d9edcc2fd19f
pbarroca <pbarroca@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-09-11 08:00:48 +0000

    Tests: introduction mock manquant
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@878 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

--- tests/application/modules/opac/controllers/CatalogueControllerTest.php
+++ tests/application/modules/opac/controllers/CatalogueControllerTest.php
@@ -21,39 +21,77 @@
 require_once 'AbstractControllerTestCase.php';
 
 class CatalogueControllerAppelMenuTest extends AbstractControllerTestCase {
+	protected $_sql;
+	protected $_sql_mock;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->_sql = Zend_Registry::get('sql');
+		$this->_sql_mock = Storm_Test_ObjectWrapper::mock()
+				->whenCalled('fetchOne')
+				->answers(null)
+
+				->whenCalled('fetchAll')
+				->answers([]);
+		
+		Zend_Registry::set('sql', $this->_sql_mock);
+	}
+
+
+	public function tearDown() {
+		Zend_Registry::set('sql', $this->_sql);
+	}
+	
+		
 	/** 
 	 *Test non regression chatenay: ecran blanc sur menu vertical
 	 *@test 
 	 */
-	function withWrongRequestShouldNotDoError500() {
-		$query = "titre=Le+Dispositif+Trace&aleatoire=1&tri=1&nb_notices=0&nb_analyse=50&id_catalogue=0&id_panier=139999&id_user=20001&reset=true";
-		$_REQUEST = array();
-		foreach (explode('&', $query) as $param) {
-			$params = explode('=', $param);
-			$_REQUEST[$params[0]] = $params[1];
-		}
+	public function withWrongRequestShouldNotDoError500() {
+		$this->_dispatchQuery("titre=Le+Dispositif+Trace&aleatoire=1&tri=1&nb_notices=0&nb_analyse=50&id_catalogue=0&id_panier=139999&id_user=20001&reset=true");
 
-		$this->dispatch('catalogue/appelmenu?'.$query);
 		$this->assertXPathContentContains('//h2', 'Aucune notice trouvée');
 	}
 
+
 	/** 
-	 *Test non regression Pontault-Combault: ecran blanc sur lien catalogue
-	 *@test 
+	 * Test non regression Pontault-Combault: ecran blanc sur lien catalogue
+	 * @test 
 	 */
-	function getFacettesSqlError() {
+	public function getFacettesSqlError() {
 		$catalogue_test = Class_Catalogue::getLoader()
 			->newInstanceWithId(151)
 			->setLibelle('nouveautés')
 			->setTypeDoc('1;3;4;5');
 
-		$query = "titre=Catalogue&aleatoire=0&tri=1&nb_notices=+&nb_analyse=&id_catalogue=151&id_panier=0&id_user=0&reset=true";
-		$this->_updateMagicREQUESTWith($query);
-		$this->dispatch('catalogue/appelmenu?'.$query, true);
+		$this->_dispatchQuery('titre=Catalogue&aleatoire=0&tri=1&nb_notices=+&nb_analyse=&id_catalogue=151&id_panier=0&id_user=0&reset=true');
+
 		$this->assertXPathContentContains('//h1', 'Catalogue');
 	}
 
 
+	/** @test */
+	public function withFacettesRequestShouldNotThrowError() {
+		$this->_dispatchQuery("titre=Catalogue%20-%20Histoire&aleatoire=0&tri=0&nb_notices=0&nb_analyse=50&id_catalogue=12&id_panier=&id_user=0&PHPSESSID=e08025e8cf614198106cc7778f021b4c&facette=A27173");
+	}
+
+
+	/**
+	 * @param $query string
+	 * @return CatalogueControllerAppelMenuTest
+	 */
+	protected function _dispatchQuery($query) {
+		$this->_updateMagicREQUESTWith($query);
+		$this->dispatch('catalogue/appelmenu?' . $query, true);
+		return $this;
+	}
+
+
+	/**
+	 * @param $query string
+	 * @return CatalogueControllerAppelMenuTest
+	 */
 	protected function _updateMagicREQUESTWith($query) {
 		$_REQUEST = array();
 		foreach (explode('&', $query) as $param) {
@@ -62,19 +100,4 @@ class CatalogueControllerAppelMenuTest extends AbstractControllerTestCase {
 		}
 		return $this;
 	}
-
-
-	/** @test */
-	public function withFacettesRequestShouldNotThrowError() {
-		$query = "titre=Catalogue%20-%20Histoire&aleatoire=0&tri=0&nb_notices=0&nb_analyse=50&id_catalogue=12&id_panier=&id_user=0&PHPSESSID=e08025e8cf614198106cc7778f021b4c&facette=A27173";
-		$_REQUEST = array();
-		foreach (explode('&', $query) as $param) {
-			$params = explode('=', $param);
-			$_REQUEST[$params[0]] = $params[1];
-		}
-
-		$this->dispatch('catalogue/appelmenu?'.$query);
-	}
 }
-
-?>
