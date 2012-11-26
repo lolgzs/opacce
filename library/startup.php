7f28458694232ffa74fc1e14ccc96dc98030bf27
llaffont <llaffont@e3cc70dd-a52f-4065-8a26-0e09943c8c5c>
2012-11-26 11:11:21 +0000

    5.37
    git-svn-id: http://afi-forge.afi-sa.fr/svn/opacce/trunk@1241 e3cc70dd-a52f-4065-8a26-0e09943c8c5c

--- library/startup.php
+++ library/startup.php
@@ -51,7 +51,7 @@ function defineConstant($name, $value) {
 
 
 function setupConstants($cfg) {
-	defineConstant('VERSION_PERGAME','V-05.36 [r'.array_at(1, explode(' ', '$Revision$ ')).']');
+	defineConstant('VERSION_PERGAME','V-05.37 [r'.array_at(1, explode(' ', '$Revision$ ')).']');
 	defineConstant('ROOT_PATH',  './');
 
 	defineConstant('MODULEDIRECTORY','./application/modules');
